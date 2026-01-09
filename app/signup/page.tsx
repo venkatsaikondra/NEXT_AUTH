370f66231f7e6ca66034c8d0ac268cc9f15fd85f
Venkat Sai Kondra <venkatsaikondra67@gmail.com>
2026-01-09 18:30:46 +0530

    added mongodb connection

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,9 +1,29 @@
+"use client"
 import React from 'react'
-
+import Link from 'next/link'
+import {useRouter} from 'next/navigation'
+import axios from 'axios'
 const signup = () => {
+    const [user,setUser]=React.useState({
+        email:"",
+        password:"",
+        username:"",
+    })
+    const onSignup()=async()=>{
+
+    }
   return (
     <div>
-      <h1>SignUp page</h1>
+      <h1>SignUp</h1>
+      <hr/>
+      <label htmlFor="username">username</label>
+      <input className=" p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600" id="username" type="text" value={user.username} onChange={(e)=>setUser({...user,username:e.target.value})} placeholder='username'></input>
+      <label htmlFor="email">email</label>
+      <input className=" p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600" id="email" type="text" value={user.email} onChange={(e)=>setUser({...user,email:e.target.value})} placeholder='email'></input>
+      <label htmlFor="password">password</label>
+      <input className=" p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600" id="password" type="password" value={user.password} onChange={(e)=>setUser({...user,password:e.target.value})} placeholder='password'></input>
+      <button onClick={onSignup} className=' p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600'>SignUp</button>
+      <Link href='/login'>Visit Login Page</Link>
     </div>
   )
 }
