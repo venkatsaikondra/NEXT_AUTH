4eeaa0404d0a1eb899f8c2726628e6bc74320593
Venkat Sai Kondra <venkatsaikondra67@gmail.com>
2026-01-10 15:49:46 +0530

    added signup route.ts backend

--- app/api/users/login/route.ts
+++ app/api/users/login/route.ts
@@ -2,5 +2,34 @@ import { connect } from "@/dbConfig/dbConfig"
 import User from "@/models/userModel"
 import { NextRequest, NextResponse } from "next/server"
 import bcryptjs from "bcryptjs"
+import { error } from "console"
 
 connect()
+
+export async function POST(request: NextRequest) {
+  try {
+    const reqBody = await request.json()
+    const { username, email, password } = reqBody
+
+    // 🔍 Check if user exists
+    const user = await User.findOne({ email })
+    if (!user) {
+      return NextResponse.json(
+        { error: "User does not exist" },
+        { status: 400 }
+      )
+    }
+    //check if password is correct
+    const validPassword=await bcryptjs.compare(password,user.password)
+    if(!validPassword){
+        return NextResponse.json({error:"Invalid Password"},{status:400})
+    }
+    //create toke data
+    
+  } catch (error: any) {
+    return NextResponse.json(
+      { error: error.message },
+      { status: 500 }
+    )
+  }
+}
